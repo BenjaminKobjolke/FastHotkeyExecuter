668c08d13d942b65a0ad918906102a1a74587190
Benjamin Kobjolke <b.kobjolke@xida.de>
2025-02-06 08:38:24 +0100

    tuning

--- FastHotkeyExecuter.bat
+++ FastHotkeyExecuter.bat
@@ -1,6 +1,9 @@
 @echo off
+REM Move to the folder containing this .bat
+cd /d %~dp0
+
 echo Activating virtual environment...
 call venv\Scripts\activate.bat
 
 echo Starting FastHotkeyExecuter...
-python src/main.py
+python src\main.py
