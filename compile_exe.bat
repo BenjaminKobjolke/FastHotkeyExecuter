09c88ce2f52ea00c997f018936cb1d514a3a8c98
Benjamin Kobjolke <b.kobjolke@xida.de>
2025-05-22 16:28:10 +0200

    FEATURE: hide console window

--- compile_exe.bat
+++ compile_exe.bat
@@ -1,4 +1,4 @@
 @echo off
 call activate_environment.bat
-call pyinstaller --name FastHotkeyExecuter --onefile main.py --add-data "data;data" --add-data "config;config"
+call pyinstaller --name FastHotkeyExecuter --onefile --windowed main.py --add-data "data;data" --add-data "config;config"
 pause
